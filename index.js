9d7e7d0ced48483c0b2cb9e9f568c8781acec02a
Adam Shiervani <adam.shiervani@gmail.com>
2016-03-07 13:03:02 +0100

    Remove module exports

--- index.js
+++ index.js
@@ -2,30 +2,28 @@ var program = require('commander');
 var express = require('express');
 var app = express();
 
-module.exports = function() {
-  program
-    .version(require('./package.json').version)
-    .usage('<dir> [options]')
-    .option('-i, --index [indexfile]', 'Specify the index file')
-    .option('-p, --port [port]', 'Specify the port number')
-    .parse(process.argv);
+program
+  .version(require('./package.json').version)
+  .usage('<dir> [options]')
+  .option('-i, --index [indexfile]', 'Specify the index file')
+  .option('-p, --port [port]', 'Specify the port number')
+  .parse(process.argv);
 
-  var opts = {
-    index: program.index || 'index.html',
-    port: program.port || '3000',
-    dir: program.args[0] || './'
-  };
+var opts = {
+  index: program.index || 'index.html',
+  port: program.port || '3000',
+  dir: program.args[0] || './'
+};
 
-  app.use(express.static(opts.dir));
-  app.use(function (req, res, next) {
-    if (req.method === 'GET' && req.accepts('html')) {
-      return res.sendFile(opts.index, {root: opts.dir}, next);
-    } else {
-      return next();
-    }
-  });
+app.use(express.static(opts.dir));
+app.use(function (req, res, next) {
+  if (req.method === 'GET' && req.accepts('html')) {
+    return res.sendFile(opts.index, {root: opts.dir}, next);
+  } else {
+    return next();
+  }
+});
 
-  app.listen(opts.port, () => {
-    console.log('Listening at localhost:' + opts.port);
-  });
-}
+app.listen(opts.port, () => {
+  console.log('Listening at localhost:' + opts.port);
+});
