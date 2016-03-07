6fc2da664ec607badb55432583f4ca5545d856b3
Adam Shiervani <adam.shiervani@gmail.com>
2016-03-07 12:28:45 +0100

    Inital

---
+++ README.md
@@ -0,0 +1,15 @@
+spa-serve
+=========
+
+A small library command-line server, tailored for Single Page Applications
+## Installation
+
+  npm i -g spa-server
+
+## Usage
+  ```spa-server```
+
+
+## Release History
+
+* 0.1.0 Initial release
---
+++ index.js
@@ -0,0 +1,30 @@
+var program = require('commander');
+var express = require('express');
+var app = express();
+
+module.exports = function() {
+  program
+    .version(require('./package.json').version)
+    .usage('<dir> [options]')
+    .option('-i, --index [indexfile]', 'Specify the index file')
+    .option('-p, --port [port]', 'Specify the port number')
+    .parse(process.argv);
+
+  var opts = {
+    index: program.index || 'index.html',
+    port: program.port || '3000',
+    dir: program.args[0] || './'
+  };
+  app.use(express.static(opts.dir));
+  app.use(function (req, res, next) {
+    if (req.method === 'GET' && req.accepts('html')) {
+      return res.sendFile(opts.index, {root: opts.dir}, next);
+    } else {
+      return next();
+    }
+  });
+
+  app.listen(port, () => {
+    console.log('Listening at localhost:' + opts.port);
+  });
+}
